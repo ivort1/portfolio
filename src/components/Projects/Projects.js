76117fb95ca91e3ed5cc3d40b3b44e7f7bad1ca8
Ivan Ortiz <ortiz.ivan93@gmail.com>
2020-05-25 18:22:18 -0700

    Still working on the ProjectCard labels.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,11 +4,24 @@ import ProjectCard from './ProjectCard';
 // CSS
 import './Projects.css'
 
+// Media
+import portfolio from './Media/portfolioScreenshot.PNG';
+
 const Projects = () => {
+    
+    let portfolioLabels = ['ReactJS', 'Material-UI', 'HTML5', 'CSS3', 'JavaScript'];
+    
     return (
         <div className='projectsFlex'>
             <h1 id='projects'>Projects</h1>
-            <ProjectCard />
+            <ProjectCard
+                imageAlt="Ivan's Portfolio"
+                image={portfolio}
+                imageTitle="Portfolio"
+                projectTitle='Portfolio'
+                projectDescription="The site you're currently on. It was built from scratch without the use of a template."
+                techStackArray={portfolioLabels}
+            />
         </div>
     );
 }
