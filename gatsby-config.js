849ecc51a1a18bb9a58cd7d66fcbaed85303610f
ivunortiz <ivan.ortiz1@ingka.ikea.com>
2021-07-06 19:13:41 -0400

    Modified gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "static/images",
+        path: "/static",
       },
       __key: "images",
     },
